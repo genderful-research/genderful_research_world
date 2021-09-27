9021ead98596661bc130b07e1a368540ffd32522
Arjen <arjen@bochoven.net>
2021-09-27 23:14:52 +0200

    Change setup

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/quiz',
+    name: 'Quiz',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Quiz.vue')
+  },
+  {
+    path: '/research/biomedical',
+    name: 'Biomedical Research',
+    component: () => import('../views/Research.vue'),
+    props: { 
+      title: "Biomedical Research",
+      areas: ["In vitro", "In vivo"]
+    }
+  },
+  {
+    path: '/research/health',
+    name: 'Health Research',
+    component: () => import('../views/Research.vue'),
+    props: { 
+      title: "Health Research",
+      areas: ["Clinical", "Population"]
+    }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Genderful Research World</h1>
+        <p>The interactive sex and gender resource platform for biomedical and health scientists.</p>
+      </div>
+    </div>
+  </div>
+</template>
