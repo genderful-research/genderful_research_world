a20eef3ad3a5af59e3584b905f806840da6e4f29
bochoven <arjen@bochoven.net>
2021-10-03 21:54:27 +0000

    Deploying to gh-pages from  @ 6ac3b52ce7a80481d0781fec1259d1068c800ff8 🚀

---
+++ js/about.b2a89ca9.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"2e44":function(e,c,n){"use strict";n.r(c);var t=n("7a23"),o={class:"container"},s=Object(t["h"])("div",{class:"row"},[Object(t["h"])("div",{class:"col"},[Object(t["h"])("h1",null,"Quiz"),Object(t["h"])("p",null,"Are you sure you know it all? Test yourself")])],-1),u=[s];function r(e,c){return Object(t["z"])(),Object(t["g"])("div",o,u)}var a=n("6b0d"),l=n.n(a);const i={},b=l()(i,[["render",r]]);c["default"]=b}}]);
+//# sourceMappingURL=about.b2a89ca9.js.map
