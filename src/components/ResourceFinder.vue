0c2a8d1ee98f3f9b6136aa979907897db49cf006
Arjen <arjen@bochoven.net>
2021-10-03 23:19:20 +0200

    Put Resourcefinder on homepage

--- src/components/ResourceFinder.vue
+++ src/components/ResourceFinder.vue
@@ -1,3 +1,46 @@
+<script setup>
+    import resources from '@/assets/resources.yml';
+    import {ref, computed} from 'vue'
+
+    const resourceFilterSelected = ref([])
+
+    // Todo: get these automatically
+    const resourceFilterOptions = [
+        {item: 'Introduction and terminology', name: 'Introduction and terminology'},
+        {item: 'Research Question and Design', name: 'Research Question and Design'},
+        {item: 'Study population and sample size', name: 'Study population and sample size'},
+        {item: 'Data Collection and Analysis', name: 'Data Collection and Analysis'},
+        {item: 'Data Interpretation and Translation to Practice', name: 'Data Interpretation and Translation to Practice'},
+        {item: 'Funding', name: 'Funding'},
+        {item: 'Team Assembly', name: 'Team Assembly'},
+        {item: 'Case Studies', name: 'Case Studies'},
+    ]
+    const filteredResults = computed(() => resources.filter(resource => {
+            return resourceFilterSelected.value.includes(resource['Research Phase'])
+    }))
+</script>
+
 <template>
-    <h2>Search Results</h2>
+<div class="row">
+    <div class="col">
+        <h3>Search Results</h3>
+        <div v-for="resource in filteredResults" :key="resource">
+            <h4>{{ resource["Description"] }}</h4>
+            <p>Research phase: {{ resource["Research Phase"] }}</p>
+            <p><a :href="resource['Resource Suggestions']">{{ resource["Resource Suggestions"] }}</a></p>
+        </div>
+    </div>
+    <div class="col">
+        <h2>Resource Filter</h2>
+        <b-form-checkbox-group
+            v-model="resourceFilterSelected"
+            :options="resourceFilterOptions"
+            stacked
+            class="mb-3"
+            value-field="item"
+            text-field="name"
+            disabled-field="notEnabled"
+        ></b-form-checkbox-group>
+    </div>
+</div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,18 @@
+<script setup>
+import ResourceFinder from "../components/ResourceFinder.vue";
+</script>
+
 <template>
   <div class="container">
     <div class="row">
       <div class="col">
         <h1>Genderful Research World</h1>
         <p>The interactive sex and gender resource platform for biomedical and health scientists.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <ResourceFinder/>
       </div>
     </div>
   </div>
