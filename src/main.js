d0582dfa28deed3d09d1099a62c5bdcf8d59b040
Arjen <arjen@bochoven.net>
2021-10-03 23:18:36 +0200

    Formatting

--- src/main.js
+++ src/main.js
@@ -6,4 +6,8 @@ import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
-createApp(App).use(store).use(router).use(BootstrapVue3).mount('#app')
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(BootstrapVue3)
+    .mount('#app')
