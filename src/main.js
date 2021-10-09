5bf33ed6325cc30bd4bd3833e8f78399691fc8d5
Arjen <arjen@bochoven.net>
2021-10-10 00:13:35 +0200

    Add markdown as mixin

--- src/main.js
+++ src/main.js
@@ -5,8 +5,18 @@ import store from './store'
 import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import marked from 'marked'
+
+const markedMixin = {
+    methods: {
+         marked: function (input) {
+            return marked (input, {gfm: true, breaks: false});
+        },
+    },
+};
 
 createApp(App)
+    .mixin(markedMixin)
     .use(store)
     .use(router)
     .use(BootstrapVue3)
