cecae74f438c17ca9af25f624aeeb854547d6d54
Arjen <arjen@bochoven.net>
2021-10-10 15:22:53 +0200

    Start quizzing

---
+++ src/components/Quiz.vue
@@ -0,0 +1,17 @@
+<script setup>
+import quiz from '@/assets/quiz.yml';
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+          <h3>{{ quiz.general.definition.question }}</h3>
+          <p>
+              
+          </p>
+    {{ quiz }}
+      </div>
+    </div>
+    </div>
+</template>
--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,10 +1,16 @@
+<script setup>
+import Quiz from '../components/Quiz.vue';
+</script>
+
 <template>
   <div class="container">
     <div class="row">
       <div class="col">
         <h1>Quiz</h1>
         <p>Are you sure you know it all? Test yourself</p>
+     
       </div>
     </div>
   </div>
+  <Quiz/>
 </template>
