f7047bde094163bf08944fe707c99c3e1f88ae85
Arjen <arjen@bochoven.net>
2021-10-10 18:20:34 +0200

    Quiz changes

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -8,9 +8,22 @@ import quiz from '@/assets/quiz.yml';
       <div class="col">
           <h3>{{ quiz.general.definition.question }}</h3>
           <p>
-              
+            <input type="radio" class="btn-check" name="definition" id="definition-yes" autocomplete="off">
+            <label class="btn btn-outline-success" for="definition-yes">Yes</label>
+
+            <input type="radio" class="btn-check" name="definition" id="definition-no" autocomplete="off">
+            <label class="btn btn-outline-danger" for="definition-no">No</label>
           </p>
-    {{ quiz }}
+          <h3>{{ quiz.general.relevance.question }}</h3>
+            <p>
+            <input type="radio" class="btn-check" name="options-outlined" id="success-outlined" autocomplete="off">
+            <label class="btn btn-outline-success" for="success-outlined">Yes</label>
+
+            <input type="radio" class="btn-check" name="options-outlined" id="danger-outlined" autocomplete="off">
+            <label class="btn btn-outline-danger" for="danger-outlined">No</label>
+          </p>
+
+    <!-- {{ quiz }} -->
       </div>
     </div>
     </div>
