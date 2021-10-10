225591f8cbb2498e2c2d35e0b0fe94aad9871037
bochoven <arjen@bochoven.net>
2021-10-10 16:21:22 +0000

    Deploying to gh-pages from @ genderful-research/genderful_research_world@f7047bde094163bf08944fe707c99c3e1f88ae85 🚀

--- js/about.3d253235.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"2e44":function(e,c,n){"use strict";n.r(c);var t=n("7a23"),o={class:"container"},s=Object(t["h"])("div",{class:"row"},[Object(t["h"])("div",{class:"col"},[Object(t["h"])("h1",null,"Quiz"),Object(t["h"])("p",null,"Are you sure you know it all? Test yourself")])],-1),u=[s];function r(e,c){return Object(t["z"])(),Object(t["g"])("div",o,u)}var a=n("6b0d"),l=n.n(a);const i={},b=l()(i,[["render",r]]);c["default"]=b}}]);
-//# sourceMappingURL=about.3d253235.js.map
