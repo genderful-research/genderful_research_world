bd52fce1c1b694df47d33f582170a7408574d3c4
Arjen <arjen@bochoven.net>
2021-10-12 20:34:23 +0200

    Add quiz questions

---
+++ src/components/Question.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {defineProps, ref} from "vue"
+defineProps({
+  item: Object,
+})
+const selected = ref()
+const answers = [
+    {text: 'Yes', value: 'yes'},
+    {text: 'No', value: 'no'},
+]
+</script>
+
+<template>
+    <p>{{ item.question }}</p>
+    <b-button-group>
+        <b-button
+            v-for="answer in answers"
+            variant="outline-primary"
+            size="lg"
+            :key="answer.value"
+            @click.stop="selected = answer.value"
+            :pressed="selected === answer.value">
+            {{ answer.text }}
+        </b-button>
+    </b-button-group>
+</template>
--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,29 +1,26 @@
 <script setup>
 import quiz from '@/assets/quiz.yml';
+// import {ref} from 'vue'
+import Question from './Question.vue';
+// const buttonsSelected = ref()
+// const question = {
+//     text: "Do you know what sex and gender are?",
+//     id: "one",
+// }
+
+
 </script>
 
 <template>
   <div class="container">
     <div class="row">
       <div class="col">
-          <h3>{{ quiz.general.definition.question }}</h3>
-          <p>
-            <input type="radio" class="btn-check" name="definition" id="definition-yes" autocomplete="off">
-            <label class="btn btn-outline-success" for="definition-yes">Yes</label>
-
-            <input type="radio" class="btn-check" name="definition" id="definition-no" autocomplete="off">
-            <label class="btn btn-outline-danger" for="definition-no">No</label>
-          </p>
-          <h3>{{ quiz.general.relevance.question }}</h3>
-            <p>
-            <input type="radio" class="btn-check" name="options-outlined" id="success-outlined" autocomplete="off">
-            <label class="btn btn-outline-success" for="success-outlined">Yes</label>
-
-            <input type="radio" class="btn-check" name="options-outlined" id="danger-outlined" autocomplete="off">
-            <label class="btn btn-outline-danger" for="danger-outlined">No</label>
-          </p>
-
-    <!-- {{ quiz }} -->
+          <div v-for="section in quiz" :key="section.title">
+              <h4>{{ section.title }}</h4>
+              <p v-for="item in section.questions" :key="item.question">
+                  <Question :item="item"/>
+              </p>
+          </div>
       </div>
     </div>
     </div>
