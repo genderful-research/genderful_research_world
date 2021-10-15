1e933578531bf1db3c86bf65ab2336252482bac1
Arjen <arjen@bochoven.net>
2021-10-16 00:03:48 +0200

    Add default view

---
+++ src/views/Default.vue
@@ -0,0 +1,13 @@
+<script setup>
+import content from '@/assets/content.yml';
+import marked from "marked"
+</script>
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+          <div v-html="marked(content.about.intro_body)"></div>
+      </div>
+    </div>
+  </div>
+</template>
