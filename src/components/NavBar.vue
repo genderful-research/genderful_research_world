3cefb9a9308388d61a1aaf3417ad4dabece0a49f
Arjen <arjen@bochoven.net>
2021-10-16 00:03:31 +0200

    Add new nav items

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,3 +1,7 @@
+<script setup>
+import content from '@/assets/content.yml';
+</script>
+
 <template>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
@@ -11,22 +15,24 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-            <router-link class="nav-link" to="/quiz">Quiz</router-link>
+            <router-link class="nav-link" to="/quiz">{{ content.navigation.quiz }}</router-link>
         </li>
         <li class="nav-item">
-            <router-link class="nav-link" to="/research/biomedical">Biomedical Research</router-link>
+            <router-link class="nav-link" to="/research">Research road</router-link>
         </li>
         <li class="nav-item">
-            <router-link class="nav-link" to="/research/health">Health Research</router-link>
+            <router-link class="nav-link" to="/funding">Funding</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" to="/assembling_teams">Assembling teams</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" to="/case_studies">Case studies</router-link>
         </li>
         <li class="nav-item">
             <router-link class="nav-link" to="/about">About</router-link>
         </li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
