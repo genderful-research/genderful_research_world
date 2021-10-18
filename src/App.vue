e15801a6fc788833b58d2bb1f6dcc16e9e6b5160
Arjen <arjen@bochoven.net>
2021-10-18 08:23:30 +0200

    Add redirect handling for gh pages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script setup>
   import NavBar from '@/components/NavBar.vue';
+  import { useRouter } from 'vue-router'
+
+  // For github pages: handle redirect from 404.html
+  const path = localStorage.getItem('path');
+  if(path) {
+    const router = useRouter()
+    localStorage.removeItem('path');
+    router.push({path: path})
+  }
 </script>
 
 <template>
