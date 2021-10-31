280cc74cc11fbebba4d637ea214684049fd3555a
Arjen <arjen@bochoven.net>
2021-10-31 11:28:59 +0100

    Make resource icon robust

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -1,5 +1,6 @@
 <script setup>
-    import resources from '@/assets/resources.csv';
+    import resources from '@/assets/resources.csv'
+    import content from '@/assets/content.yml'
     import {defineProps, computed, toRefs} from 'vue'
     const props = defineProps({
         road: String,
@@ -13,16 +14,10 @@
         }
         return resource['phase'] === phase.value
     }))
-    const typeToIcon = (type) => {
-        const iconList = {
-            factsheet: "bi-check-square",
-            article: "bi-newspaper",
-            infographic: "bi-image",
-            video: "bi-camera-video",
-            guideline: "bi-card-text",
-        }
-        return iconList[type]
-    }
+
+    const getIcon = (resource) => content.resource_types[resource.type] ? content.resource_types[resource.type].icon : "bi-question-diamond"
+    const getText = (resource) => content.resource_types[resource.type] ? content.resource_types[resource.type].text : "Unknown"
+
 </script>
 
 <template>
@@ -30,11 +25,14 @@
     <h3>Relevant resources</h3>
     <div v-for="resource in filteredResults" :key="resource">
         <div class="card mb-3">
+            <div class="card-header">
+                <i :class="getIcon(resource)"></i>
+                {{ getText(resource) }}
+            </div>
             <div class="card-body">
-            <h5 class="card-title">{{ resource.title }} <i style="font-size: 2rem" :class="typeToIcon(resource.type)"></i></h5>
+            <h5 class="card-title">{{ resource.title }}</h5>
             <p class="card-text">{{ resource.description }}</p>
-            <p class="card-text">{{ resource.type }}</p>
-            <a target="_blank" class="btn btn-primary stretched-link" :href="resource.url">View</a>
+            <a target="_blank" class="btn btn-secondary stretched-link" :href="resource.url">View</a>
             </div>
         </div>
     </div>
