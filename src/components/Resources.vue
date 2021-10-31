a9938f9a62ebe9f74ade5dad012b34c72a093d3f
Arjen <arjen@bochoven.net>
2021-10-31 08:49:34 +0100

    Use csv in resource filter

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -1,17 +1,17 @@
 <script setup>
-    import resources from '@/assets/resources.yml';
+    import resources from '@/assets/resources.csv';
     import {defineProps, computed, toRefs} from 'vue'
     const props = defineProps({
         road: String,
         phase: String,
     })
     const { phase } = toRefs(props);
-    console.log(phase.value)
+    console.log(resources)
 
 
     const filteredResults = computed(() => resources.filter(resource => {
         console.log(phase.value)
-            return resource['Research Phase'] == phase.value
+            return resource['phase'] == phase.value
     }))
 </script>
 
@@ -19,7 +19,7 @@
 
     <h3>Relevant resources</h3>
     <div v-for="resource in filteredResults" :key="resource">
-        <h6>{{ resource["Description"] }}</h6>
-        <p>🔗 <a target="_blank" :href="resource['Resource Suggestions']">Link to the resource</a></p>
+        <h6>{{ resource["title"] }}</h6>
+        <p>🔗 <a target="_blank" :href="resource['url']">Link to the resource</a></p>
     </div>
 </template>
