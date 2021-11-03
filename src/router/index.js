f9c28e4ed256819fcfcb086f1c2140cd42357aa5
Arjen <arjen@bochoven.net>
2021-11-03 22:46:34 +0100

    Add scrollup to router

--- src/router/index.js
+++ src/router/index.js
@@ -76,7 +76,10 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior() {
+    document.getElementById('app').scrollIntoView();
+  }
 })
 
 export default router
