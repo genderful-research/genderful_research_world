e3866740288ca7cde140e8d5c52fcec61bd7283d
Arjen <arjen@bochoven.net>
2021-11-07 16:25:41 +0100

    Redirect to default phase

--- src/views/Research.vue
+++ src/views/Research.vue
@@ -2,13 +2,27 @@
 import content from '@/assets/content.yml';
 import ResearchRoad from '../components/ResearchRoad.vue';
 import CardLinks from "../components/CardLinks.vue";
-import {defineProps} from "vue"
+import {defineProps, toRefs, onUpdated} from "vue"
 import Resources from '../components/Resources.vue';
-defineProps({
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const props = defineProps({
   road: String,
   location: String,
 })
-  const buttons = [
+const { road, location } = toRefs(props);
+const updateLocation = () => {
+  if (road.value && !location.value) {
+    router.push('/research/' + road.value + '/research-question')
+  }
+}
+updateLocation()
+onUpdated(() => {
+  updateLocation()
+})
+
+const buttons = [
       [
           {content: content.biomedical, url: "/research/biomedical"},
           {content: content.health, url: "/research/health"},
