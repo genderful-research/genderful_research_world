40a725b7c7098c4a4ccff8d14765f8d275481b67
Arjen van Bochoven <arjen@bochoven.net>
2021-11-08 17:29:26 +0100

    Update funded by

--- src/components/FundedBy.vue
+++ src/components/FundedBy.vue
@@ -2,13 +2,17 @@
 import content from '@/assets/content.yml';
 </script>
 <template>
-    <h4>{{ content.funded_by.title }}</h4>
-    <p>
-        <img src="@/assets/zonmw-logo.jpg" class="img-fluid" alt="ZonMw Logo">
-        ZonMw (Netherlands)
-    </p>
-    <p>
-        <img src="@/assets/cihr-logo.png" class="img-fluid" alt="ZonMw Logo">
-        CIHR-IGH (Canada)
-    </p>
+<h4>{{ content.funded_by.title }}</h4>
+<div class="card-group">
+    <div class="card">
+        <div class="card-body">
+            <img src="@/assets/cihr-logo.png" class="card-img-top " alt="CIHR-IGH Logo">
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <img src="@/assets/zonmw-logo.png" class="card-img-top" alt="ZonMw Logo">
+        </div>
+    </div>
+</div>
 </template>
