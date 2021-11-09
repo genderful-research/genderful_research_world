d83535aa4d2506b65a08b608282944a9f4d31262
Arjen van Bochoven <arjen@bochoven.net>
2021-11-09 11:48:29 +0100

    Add bootswatch - set sketchy

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootswatch/dist/sketchy/bootstrap.min.css'
 
 createApp(App)
     .use(store)
