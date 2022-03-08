1606a2caa21854370fe879ec664719c93a6238ac
Arjen <arjen@bochoven.net>
2022-03-08 20:56:01 +0100

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,7 @@
 <template>
     <footer class="footer mt-auto py-3 bg-light">
-  <div class="container">
-   <router-link class="btn btn-outline-secondary btn-md" to="/about">
-   About us
-    </router-link>
+  <div class="container text-end">
+    Genderful Research World ©2022 
   </div>
 </footer>
 </template>
