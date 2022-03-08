9d24435585a8e289c36b5b6d04831bedf39b3eeb
Arjen <arjen@bochoven.net>
2022-03-08 20:30:29 +0100

    Remove map from footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
     <footer class="footer mt-auto py-3 bg-light">
   <div class="container">
-   <router-link class="btn btn-outline-secondary btn-lg" to="/">
-    <img src="@/assets/map_small.png" alt="Small map"> Back to the map
+   <router-link class="btn btn-outline-secondary btn-md" to="/about">
+   About us
     </router-link>
   </div>
 </footer>
